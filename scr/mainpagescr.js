459b8bb572d040bef6ea0d8a6fb0bb5adab1976a
ReeeoooN <reon.sem@gmail.com>
2022-11-26 12:00:29 +0700

    Простенькое создание тредов

--- scr/mainpagescr.js
+++
@@ -1,5 +0,0 @@
-const fio = document.querySelector("fio")
-
-let username = 'Фамилия имя отчество'
-
-fio.appendChild(username)
---
+++ scr/treadpagescr.js
@@ -0,0 +1,40 @@
+const btn = document.querySelector('.treadsend')
+
+btn.onclick = ()=>{
+    const treadtext = document.querySelector('.treadcreate')
+    const treadfield = document.querySelector('.treadblock')
+    let newtread = document.createElement('div');
+    newtread.className = 'mainblock'
+    treadfield.prepend(newtread)
+    let autor = document.createElement('div')
+    autor.className = 'creator'
+    newtread.append(autor)
+    let name = document.createElement('div')
+    name.className = 'uname'
+    name.innerHTML = 'Name'
+    autor.append(name)
+    let date = document.createElement('div')
+    date.className = 'treaddate'
+    date.innerHTML = 'date'
+    autor.append(date)
+    let content = document.createElement('div')
+    content.className = 'treadcontent'
+    newtread.append(content)
+    let textcontent = document.createElement('p')
+    textcontent.innerHTML = treadtext.value
+    content.append(textcontent)
+    let tegs = document.createElement('div')
+    tegs.className = 'tegs'
+    newtread.append(tegs)
+    let teg = document.createElement('div')
+    teg.innerHTML = 'Тег 2'
+    tegs.append(teg)
+    let comments = document.createElement('div')
+    comments.className = 'treadcomments'
+    comments.innerHTML = 'Комментарии'
+    newtread.append(comments)
+    let eye = document.createElement('i')
+    eye.className = 'fa fa-eye'
+    comments.append(eye)
+    treadtext.value = ''; 
+}
